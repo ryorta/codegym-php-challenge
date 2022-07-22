566af5f6767ca51030fece189ecf0d1825bb4d89
ryorta <ryotabaske13@gmail.com>
2022-07-22 02:13:09 +0000

    committed

--- html/functions.php
+++ html/functions.php
@@ -55,26 +55,76 @@ function createTweet($text, $user_id)
  */
 function getTweets()
 {
-    $sql = 'select t.id, t.text, t.user_id, t.created_at, t.updated_at, u.name';
+    $sql = 'select t.id, t.text, t.user_id, t.created_at, t.updated_at,  u.name';
     $sql .= ' from tweets t join users u on t.user_id = u.id';
     $sql .= ' order by t.updated_at desc';
     $stmt = getPdo()->prepare($sql);
     $stmt->execute();
+    /*echo "<pre>";
+    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+    echo "</pre>";*/
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-/* いいねを押すことでdbのfavoriteテーブルにデータが入るような関数をつくる。 */
+/* いいねを押すことでdbのfavoriteテーブルにデータが入るような関数をつくる。*/
 function createFavorite($member_id, $post_id)
 {
     $sql = 'insert into favorites (member_id, post_id, created_at, updated_at)';
     $sql .= ' values (:member_id, :post_id, :created_at, :updated_at)';
     $now = date("Y-m-d H:i:s");
     $stmt = getPdo()->prepare($sql);
-    $stmt->bindValue(':member_id', $member_id, PDO::PARAM_INT);
+    $stmt->bindValue(':member_id', $member_id, PDO::PARAM_STR);
     $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
     $stmt->bindValue(':created_at', $now, PDO::PARAM_STR);
     $stmt->bindValue(':updated_at', $now, PDO::PARAM_STR);
     return $stmt->execute();
 }
 
-/* いいねを取り消すことでdbからもデータが削除されるような関数。 */
+/* いいねを取り消すことでdbからもデータが削除されるような関数。*/
+function deleteFavorite($member_id, $post_id)
+{
+    $sql = 'delete from favorites';
+    $sql .= ' where member_id = :member_id and post_id = :post_id';
+    $stmt = getPdo()->prepare($sql);
+    $stmt->bindValue(':member_id', $member_id, PDO::PARAM_INT);
+    $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+    return $stmt->execute();
+}
+/*いいねを表示するためのgetTweetのような関数が必要?*/
+/**
+ * @param string $user_id ユーザー名
+ * @return array 自分がいいねした投稿IDの配列
+ */
+function isMyfavorite($member_id, $post_id)
+{
+    $sql = 'select post_id';
+    $sql .= ' from favorites';
+    $sql .= ' where member_id = :member_id and post_id = :post_id';
+    $sql .= ' order by updated_at desc';
+    $stmt = getPdo()->prepare($sql);
+    $stmt->bindValue(':member_id', $member_id, PDO::PARAM_INT);
+    $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC); //DBMSからPHP側に取り出す。結果表の消える。
+    // この時点では、$dataは配列の中に連想配列1つが入っている状態。
+    
+    $fabList = array();
+    //入れ替え
+    foreach ($data as $d) {
+        $fabList = $d;
+    }
+    
+    return $fabList; //一次元配列
+}
+
+function countFavorites($post_id)
+{
+    $sql = 'select post_id';
+    $sql .= ' from favorites';
+    $sql .= ' where post_id = :post_id';
+    $stmt = getPdo()->prepare($sql);
+    $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $fav = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return count($fav);
+}
--- html/index.php
+++ html/index.php
@@ -21,8 +21,6 @@ function newtweet($tweet_textarea)
     createTweet($tweet_textarea, $_SESSION['user_id']);
 }
 
-//いいねを押したらdbのfavoriteテーブルにデータが入るような関数を呼び出す。
-
 //いいねを取り消すことでdbのfavoriteテーブルからデータを削除する関数を呼び出す。
 
 /**
@@ -38,14 +36,32 @@ if ($_POST) { /* POST Requests */
     if (isset($_POST['logout'])) { //ログアウト処理
         logout();
         header("Location: login.php");
-    } else if (isset($_POST['tweet_textarea'])) { //投稿処理
+    } elseif (isset($_POST['tweet_textarea'])) { //投稿処理
         newtweet($_POST['tweet_textarea']);
-        header("Location: index.php");
-    } else if(isset($_POST['post_id'])) { //投稿処理
-        newfavorite($_POST['post_id']);
         header("Location: index.php");
     }
 }
+
+/*GETの判定*/
+if ($_GET) {
+    if (isset($_GET['favorite'])) { //リクエストパラメータにfavoriteがあるか？
+        if (isMyfavorite($_SESSION['user_id'], $_GET['favorite'])) {
+            deleteFavorite($_SESSION['user_id'], $_GET['favorite']);
+        } else {
+            createFavorite($_SESSION['user_id'], $_GET['favorite']);
+        }
+        header("Location: index.php");
+        exit; //これを入れるだけで削除して作り直すっていうのはなくなると思う。
+        //if-elseを使うのも有り。
+    }
+}    //DBにあるか？該当の投稿IDがfavoriteテーブルにあるか？
+    //ある
+    //deleteを発行する関数を実行　関数（投稿ID,SESSION['user_id']）
+    //リダイレクト
+    //ない
+    //insertを発行する関数を実行　関数（投稿ID,SESSION['user_id']）//createUserを参考
+    //リダイレクト
+
 
 $tweets = getTweets();
 $tweet_count = count($tweets);
@@ -73,20 +89,35 @@ $tweet_count = count($tweets);
       </div>
     </div>
     <h1 class="my-5">コメント一覧</h1>
-    <?php foreach ($tweets as $t) { ?>
+    <?php foreach ($tweets as $t) {
+    ?>
       <div class="card mb-3">
         <div class="card-body">
           <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
           <p class="card-text"><?= "{$t['text']}" ?></p>
           <!--dbにデータが入っているなら、特に「member_id」に値が入っている(つまり自分が押したいいね)ものには、赤いハートが表示されるようにする。
-          そうでないなら灰色。
-          また、「post_id」にデータが入っているもの、つまり誰かしらによっていいねが押されている状態のものに関しては、ハートの横にいいね数(count($post_id=○○))を表示する。-->
+          そうでないなら灰色。-->
+        <?php
+          if (isMyfavorite($_SESSION['user_id'], $t['id'])) {
+              ?>
+          <a href="index.php?favorite=<?= "{$t['id']}" ?>"><img class="favorite-image" src='/images/heart-solid-red.svg'></a>
+        <?php
+          } else {
+              ?>
+          <a href="index.php?favorite=<?= "{$t['id']}" ?>"><img class="favorite-image" src='/images/heart-solid-gray.svg'></a>
+        <?php
+          }
+    echo countFavorites($t['id']); ?> 
+    
+          <!--また、「post_id」にデータが入っているもの、つまり誰かしらによっていいねが押されている状態のものに関しては、ハートの横にいいね数(count($post_id=○○))を表示する。-->
           <!--返信課題はここから修正しましょう。-->
           <!--<p>[返信する] [返信元のメッセージ]</p>-->
           <!--返信課題はここまで修正しましょう。-->
         </div>
       </div>
-    <?php } ?>
+    <?php
+}
+     ?><!--foreach ($tweets as $t)の終点-->
     <form method="POST">
       <input type="hidden" name="logout" value="dummy">
       <button class="btn btn-primary">ログアウト</button>
